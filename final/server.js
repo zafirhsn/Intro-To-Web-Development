84f31a68d9b71bfec3e2d45a07cc475147cbdc1a
Zafir Hasan <zafirhsn@gmail.com>
2017-12-02 19:59:01 -0500

    Got data

--- final/server.js
+++ final/server.js
@@ -1,9 +1,21 @@
 var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(8888);
+function onRequest(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile("./home.html", null, function(error, data) {
+		if (error) {
+			res.writeHead(404);
+			res.write("file not found");
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});	
+}
+
+
+http.createServer(onRequest).listen(8888);
 
 // JavaScript Document
