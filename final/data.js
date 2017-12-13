6775661f954503cbc83e248290e7ffaac07fd528
Zafir Hasan <zafirhsn@gmail.com>
2017-12-13 00:32:42 -0500

    ZH

--- final/data.js
+++ final/data.js
@@ -1,33 +1,25 @@
 //GET USER'S TOP 10 TRACKS
-function getTopTracks(access_token) {
-	$.ajax({
-		url: 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10&offset=0',
-		headers: {
-		  'Authorization': 'Bearer ' + access_token
-		},
-		success: function(response) {
-		  console.log("PRINTING response from top tracks");
-		  console.log(response);
-		  for(var i = 0; i < 10; i++) {
-			  console.log(response.items[i].name);
-		  }  
-		}
-	});
-}
+function getTopTracksLong(access_token, template_id) {
+			   $.ajax({
+			       url: 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10&offset=0',
+			       headers: {
+			           'Authorization': 'Bearer ' + access_token
+			       },
+			       success: responseFunction
+			   });
+			}
 
+			function responseFunction(response) {
+			   console.log("PRINTING response from top tracks");
+			   console.log(response);
+			   var top_tracks = "";
+			   for (var i = 0; i < 10; i++) {
+			       console.log(response.items[i].name);
 
-//GET USER'S TOP 10 ARTISTS
-function getTopArtists(access_token) {
-	$.ajax({
-		url: 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=10&offset=0',
-		headers: {
-		  'Authorization': 'Bearer ' + access_token
-		},
-		success: function(response) {
-		  console.log("PRINTING response from top tracks");
-		  console.log(response);
-		  console.log(response.items[0].name);
-		}
-	});
-}
+			   }
+			   TopTracksLong = response;
+			   console.log("PRINTING saved JSON");
+			   console.log(TopTracksLong);
+			   $("#top-tracks-long").html(template(response));
+			}
  
